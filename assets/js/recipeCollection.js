95ac78751d59106ec5bc25aab81b968febb9ddf6
DimiGreens <10632176@ucn.dk>
2025-04-20 16:43:59 +0200

    Opsat mere JS på opskriftsamlinger

--- assets/js/recipeCollection.js
+++ assets/js/recipeCollection.js
@@ -2,9 +2,30 @@ const domain = "https://mmd2-api.r-dalsgaard.dk/";
 const endPoint = "wp-json/wp/v2/posts";
 const getRealImageUrls = "?acf_format=standard";
 const getRealImageUrlsPlus = "&acf_format=standard";
-const collectionTaxonomi = "?taxonomi=opskriftsamling"
 
-fetch(domain + endPoint + collectionTaxonomi + getRealImageUrlsPlus)
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => console.log(err))
+const recipeCollectionEl = document.querySelector(".recipeCollections");
+let choosenCollection;
+
+const springButton = document.querySelector("#springCollection");
+springButton.addEventListener("click", () => {
+    fetch(domain + endPoint + "?opskriftsamling=38&per_page=40")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+})
+
+const easyButton = document.querySelector("#easyCollection");
+easyButton.addEventListener("click", () => {
+    fetch(domain + endPoint + "?opskriftsamling=39&per_page=40")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+})
+
+const kidButton = document.querySelector("#childfriendlyCollection");
+kidButton.addEventListener("click", () => {
+    fetch(domain + endPoint + "?opskriftsamling=40&per_page=40")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+})
